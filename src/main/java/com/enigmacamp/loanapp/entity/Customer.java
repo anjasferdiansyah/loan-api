67a0d6bb3c31de5793dc2131bb57db8546098234
Mohammad Anjas Ferdiansyah <anjasferdiansyah123@gmail.com>
2024-09-20 09:33:04 +0700

    feat : add another configure and add Customer Entity

---
+++ src/main/java/com/enigmacamp/loanapp/entity/Customer.java
@@ -0,0 +1,36 @@
+package com.enigmacamp.loanapp.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "mst_customer")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "date_of_birth")
+    private Date dateOfBirth;
+
+    @Column
+    private String phone;
+    @Column
+    private String status;
+}
---
+++ src/main/java/com/enigmacamp/loanapp/entity/Role.java
@@ -0,0 +1,24 @@
+package com.enigmacamp.loanapp.entity;
+
+import com.enigmacamp.loanapp.constant.ERole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "t_role")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Enumerated(EnumType.STRING)
+    private ERole role;
+}
